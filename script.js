bbfa1927db0dc59b25fefb410b8a09e61a6d6726
pelzmagic <akinloyepelz@gmail.com>
2024-12-12 14:42:32 +0100

    update style to the stacked cards animation

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const thirdBox = document.querySelector(".third_box");
 const fourthBox = document.querySelector(".fourth_box");
 const box = document.querySelector(".box");
 
-// const obsCallBack = function (entries observer) {
-//     const [entry] =
-// };
+const obsCallBack = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  // if (!entry.isIntersecting) return;
+  // entry.target.classList.add("styled_second_box");
+  // observer.unobserve(entry.target);
+};
 
 const obsOptions = {
   root: null,
-  threshold: 0.1,
+  threshold: 1.0,
 };
 
-// const observer = new IntersectionObserver(obsCallBack, obsOptions);
-// observer.observe(firstBox);
+const observer = new IntersectionObserver(obsCallBack, obsOptions);
+observer.observe(secondBox);
